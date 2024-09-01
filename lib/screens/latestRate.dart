3e5c448649c16b04e9a3bbf557b534a616e4eb6c
pongpakon <phongphakonj65@nu.ac.th>
2024-09-01 20:47:24 +0700

    Create latestRate.dart

---
+++ lib/screens/latestRate.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:simple_app/Rate.dart';
+
+class LatestRate extends StatefulWidget {
+  const LatestRate({super.key});
+
+  @override
+  State<LatestRate> createState() => _LatestRateState();
+}
+
+class _LatestRateState extends State<LatestRate> {
+
+  @override
+  initState() {
+    super.initState();
+    print("Init State");
+    getRate();
+  }
+  Future<Rate> getRate() async {
+    var params = {
+      "base": "THB"
+    };
+    var uri = Uri.https("currency-converter-pro1.p.rapidapi.com", "/latest-rates", params);
+
+    var result = await http.get(uri, headers: {
+      "X-RapidAPI-Host": "currency-converter-pro1.p.rapidapi.com",
+      "x-rapidapi-key": "C05GBKbyg0mshuD0FmkuTgEJe7mQp1QktrHjsndVBQ98Lef6WS"
+    });
+
+    Rate rate = rateFromJson(result.body);
+    print(rate.toString());
+    return rate;
+  }
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: getRate(), 
+      builder: (BuildContext context, AsyncSnapshot<Rate> snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          if (snapshot.hasError) {
+            return Center(
+              child: Text("Error: ${snapshot.error}"),
+            );
+          }
+          return Center(
+            child: Text("Rate: ${snapshot.data!.toString()}"),
+          );
+        } else {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      });
+  }
+}
